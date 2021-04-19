c6b3d9d439d2e869d46c08513822eb5c69c7d603
BRvanHogen <brvanhogen@hotmail.nl>
2021-04-19 11:51:54 +0200

    CommentController class geschreven.

---
+++ src/main/java/com/backend/eindopdracht/musictool/controller/CommentController.java
@@ -0,0 +1,22 @@
+package com.backend.eindopdracht.musictool.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/comments")
+public class CommentController {
+
+//    @Autowired
+//    private CommentService commentService;
+
+    @GetMapping(value = "")
+    public ResponseEntity<Object> getComments() {
+        return ResponseEntity.ok().body(commentService.getComments());
+    }
+}
+}
